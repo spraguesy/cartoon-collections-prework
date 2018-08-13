269b277ba724742113c5cf7d55a4e885c7d69555
Andrew Sprague <andrewsprague@outlook.com>
2018-08-13 20:42:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,5 +23,5 @@ end
 def find_the_cheese (type)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.first(type)
+  cheese_types.include?(type)
 end
